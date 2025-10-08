3d1917f668105efa2cb895678af2d6c1f5f86302
jjackson112 <jjackson11211@gmail.com>
2025-10-08 14:32:10 -0400

    Tailwind config

---
+++ config.js
@@ -0,0 +1,12 @@
+/* Control center for all Tailwind styling */
+/* Key value pairing */
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                my_teal: '#008080'
+            }
+        }
+    }
+}
